334350fb1c6462c9ee00153472af46a0f069b015
Noble-hub-cyber <nobezugo@gmail.com>
2026-03-02 16:17:30 +0100

    Add GPA calculator code

---
+++ GPACalculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class GPACalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter total quality points: ");
+        double totalQualityPoints = input.nextDouble();
+
+        System.out.print("Enter total credit hours: ");
+        double totalCreditHours = input.nextDouble();
+
+        double gpa = totalQualityPoints / totalCreditHours;
+
+        System.out.printf("Your GPA is: %.2f\n", gpa);
+    }
+          }
